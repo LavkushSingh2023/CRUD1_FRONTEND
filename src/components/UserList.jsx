8f0986982b0a83b5ac9f961066cb960e588337a5
Lavkush Singh <t21425814@gmail.com>
2025-06-14 13:14:32 +0530

    ok

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -25,7 +25,16 @@ const UserList = ({ users, fetchUsers, setEditingUser }) => {
             <tr key={u._id}>
               <td>{u.name}</td>
               <td>{u.email}</td>
-              <td><img className='cursor-pointer' src={`http://localhost:5000/uploads/${u.file}`} alt="" width="50" /></td>
+              <td>
+                {(u.file?.endsWith('.jpg') || u.file?.endsWith('.png')) ? (
+                    <img className="cursor-pointer" src={`http://localhost:5000/uploads/${u.file}`} alt="" width="50" />
+               
+                ) : (
+                    <a href={`http://localhost:5000/uploads/${u.file}`} target="_blank" rel="noreferrer">
+                    📄 View here
+                    </a>
+                 )}
+                </td>
               <td>
                 <button className="btn btn-sm btn-info me-2" onClick={() => setEditingUser(u)}>Edit</button>
                 <button className="btn btn-sm btn-danger" onClick={() => deleteUser(u._id)}>Delete</button>
